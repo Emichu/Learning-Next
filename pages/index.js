3461f35cb4919a2a884006c633b907db2f2edd84
Emi Strange <k.kreplewicz;@gmail.com>
2022-02-07 12:27:01 +0000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
-export default function Home() {
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+
+export default function HomePage(props) {
   return (
     <div>
-      <p>aids</p>
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
